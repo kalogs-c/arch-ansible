8af08ee6c7fd4cc2083ad86f41eb84a11d024230
Carlos Camilo <carloscamilocontato@gmail.com>
2023-07-29 16:31:15 -0300

    fix

--- setup.sh
+++ setup.sh
@@ -1,13 +1,24 @@
-echo "Install Ansible"
-pacman -S ansible
+if ! command -v ansible-playbook > /dev/null; then
+    echo "Install Ansible"
+    pacman -S ansible
+fi
 
 echo "Running Ansible Playbook"
 ansible-playbook ansible/playbook.yaml
 
-echo "Download Nodejs using asdf"
-asdf plugin add nodejs
-asdf install nodejs latest
-asdf global nodejs latest
+if ! command -v npm > /dev/null; then
+    echo "Download Nodejs using asdf"
+    asdf plugin add nodejs
+    asdf install nodejs latest
+    asdf global nodejs latest
+fi
 
-echo "Opening Neovim"
-vim
+if command -v gh > /dev/null; then
+    echo "Auth to github cli"
+    gh auth login
+fi
+
+if command -v gcloud > /dev/null; then
+    echo "Auth to gcloud cli"
+    gcloud init
+fi
